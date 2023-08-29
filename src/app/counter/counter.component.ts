44fa19f374e9d95b3db8eced497c284abd2be8e2
alelyQuispe <alely.quispe@vallegrande.edu.pe>
2023-08-28 21:43:57 -0500

    Hello World

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css'],
+})
+export class CounterComponent {
+  counter = 0;
+  activeDecrease = false;
+
+  increase() {
+    this.counter++;
+    this.activeDecrease = true; 
+  }
+decrease() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
+    if (this.counter === 0) {
+      this.activeDecrease = false; 
+    }
+  }
+
+  inactiveDecrease() {
+    this.activeDecrease = !this.activeDecrease;
+  }
+}
